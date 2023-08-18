c7028a533e2bca909ae87c7b49932ffb3fd54997
Pranav <pranavnarang1997@gmail.com>
2023-08-18 13:36:56 +0530

    Box-Shadow for cards

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,16 +1,29 @@
 import React from "react";
 import "./Card.css";
+import { useDispatch } from "react-redux";
+import { setMovie } from "../features/movieSlice";
 
 const Card = ({ image, title, rating, description, id }) => {
+  const dispatch = useDispatch();
   const getMovie = () => {
-    console.log(id);
+    dispatch(
+      setMovie({
+        id: id,
+        title: title,
+        image: image,
+        rating: rating,
+        description: description,
+      })
+    );
   };
   return (
     <div className="card" onClick={getMovie}>
       <img src={image} alt="" />
       <div className="card__movieHeader">
-        <p>{title}</p>
-        <p className="card__rating">{rating}</p>
+        <p className="card__title">{title}</p>
+        <p className="card__rating">
+          IMDB <span>{rating}</span>/10
+        </p>
       </div>
       <div className="card__movieDesc">
         <p>{description}</p>
