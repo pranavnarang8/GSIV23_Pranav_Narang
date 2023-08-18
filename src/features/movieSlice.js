57f41aee5b8e39fb63cc3099bb7eadb7e34a3bc4
Pranav <pranavnarang1997@gmail.com>
2023-08-18 12:58:26 +0530

    Redux Setup

---
+++ src/features/movieSlice.js
@@ -0,0 +1,22 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export const movieSlice = createSlice({
+  name: "movie",
+  initialState: {
+    movie: null,
+  },
+  reducers: {
+    setMovie: (state, action) => {
+      state.movie = action.payload;
+    },
+    resetMovie: (state) => {
+      state.movie = null;
+    },
+  },
+});
+
+export const { setMovie, resetMovie } = movieSlice.actions;
+
+export const selectMovie = (state) => state.movie.movie;
+
+export default mailSlice.reducer;
