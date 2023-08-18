82bdefc2b34850df32199a548516e9ee5868380f
Pranav <pranavnarang1997@gmail.com>
2023-08-18 19:29:47 +0530

    Infinite Scroll for cards

--- src/components/List.js
+++ src/components/List.js
@@ -4,10 +4,27 @@ import axios from "../axios";
 import { useState } from "react";
 import { useEffect } from "react";
 import Card from "./Card";
+import InfiniteScroll from "react-infinite-scroll-component";
 
 const List = ({ fetchUrl }) => {
   const [movies, setMovies] = useState([]);
+  const [page, setPage] = useState(2);
   const base_url = "https://image.tmdb.org/t/p/original/";
+
+  const fetchMoreMovies = async () => {
+    const request = await axios.get(`${fetchUrl}&page=${page}`);
+    const dataArray = request.data.results;
+
+    dataArray.sort((a, b) => {
+      const dateA = new Date(a.release_date);
+      const dateB = new Date(b.release_date);
+      return dateB - dateA;
+    });
+
+    setMovies([...movies, ...dataArray]);
+    setPage(page + 1);
+    return request;
+  };
 
   useEffect(() => {
     const fetchData = async () => {
@@ -29,18 +46,27 @@ const List = ({ fetchUrl }) => {
 
   return (
     <div className="list">
-      <div className="list__cardContainer">
-        {movies.map((movie) => (
-          <Card
-            image={`${base_url}${movie.backdrop_path}`}
-            title={movie.original_title}
-            rating={movie.vote_average}
-            description={movie.overview}
-            key={movie.id}
-            id={movie.id}
-          />
-        ))}
-      </div>
+      <InfiniteScroll
+        dataLength={movies.length}
+        next={fetchMoreMovies}
+        hasMore={page !== 24}
+      >
+        <div className="list__cardContainer">
+          {movies.map(
+            (movie) =>
+              movie.backdrop_path && (
+                <Card
+                  image={`${base_url}${movie.backdrop_path}`}
+                  title={movie.title}
+                  rating={movie.vote_average}
+                  description={movie.overview}
+                  key={movie.id}
+                  id={movie.id}
+                />
+              )
+          )}
+        </div>
+      </InfiniteScroll>
     </div>
   );
 };
