df84214dcd07df063069b68d6fc0a4e970b733c6
Pranav <pranavnarang1997@gmail.com>
2023-08-18 16:07:36 +0530

    React Router Setup

--- src/App.js
+++ src/App.js
@@ -3,12 +3,24 @@ import "./App.css";
 import Header from "./components/Header";
 import List from "./components/List";
 import requests from "./requests";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Detail from "./components/Detail";
 
 function App() {
   return (
     <div className="app">
-      <Header />
-      <List fetchUrl={requests.fetchUpcoming} />
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <Header />
+            <List fetchUrl={requests.fetchUpcoming} />
+          </Route>
+          <Route exact path="/detail">
+            <Header />
+            <Detail />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
