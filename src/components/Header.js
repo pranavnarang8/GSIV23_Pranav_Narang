8b5489374391a64e7ca55827c5f377fc27512dd9
Pranav <pranavnarang1997@gmail.com>
2023-08-19 16:39:02 +0530

    Header arrow back icon

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,12 +3,14 @@ import "./Header.css";
 import SearchIcon from "@mui/icons-material/Search";
 import HomeIcon from "@mui/icons-material/Home";
 import { useState } from "react";
-import { useDispatch } from "react-redux";
-import { resetFilter, setFilter } from "../features/movieSlice";
+import { useDispatch, useSelector } from "react-redux";
+import { resetFilter, setFilter, setMovie } from "../features/movieSlice";
+import ArrowBackIcon from "@mui/icons-material/ArrowBack";
 
 const Header = () => {
   const [input, setInput] = useState();
   const dispatch = useDispatch();
+  const movie = useSelector(setMovie);
 
   const handleSearch = (e) => {
     e.preventDefault();
@@ -20,16 +22,20 @@ const Header = () => {
   return (
     <div className="header">
       <div className="header__search">
-        <form>
-          <SearchIcon className="header__searchIcon" onClick={handleSearch} />
-          <input
-            type="text"
-            placeholder="Search"
-            value={input}
-            onChange={(e) => setInput(e.target.value)}
-          />
-          <button type="Submit" onClick={handleSearch}></button>
-        </form>
+        {!movie ? (
+          <form>
+            <SearchIcon className="header__searchIcon" onClick={handleSearch} />
+            <input
+              type="text"
+              placeholder="Search"
+              value={input}
+              onChange={(e) => setInput(e.target.value)}
+            />
+            <button type="Submit" onClick={handleSearch}></button>
+          </form>
+        ) : (
+          <ArrowBackIcon className="header__arrowBackIcon" />
+        )}
       </div>
       <HomeIcon className="header__homeIcon" />
     </div>
